699f74e612e479b46f5f67d5ced372b7236a6047
imran <imrankhannn81@gmail.com>
2021-04-22 17:34:00 +0500

    added selenium

--- spider.py
+++ spider.py
@@ -1,19 +1,43 @@
 import scrapy
+from shutil import which
+from scrapy_selenium import SeleniumRequest
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    start_urls = [
-        'http://quotes.toscrape.com/tag/humor/',
-    ]
+
+    a=2
+    custom_settings = {
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy_selenium.SeleniumMiddleware': 800
+        },
+        'SELENIUM_DRIVER_NAME': 'chrome',
+        'SELENIUM_DRIVER_EXECUTABLE_PATH': 'C:\\Users\\me\\Downloads\\chromedriver_win32\\chromedriver.exe',
+        'SELENIUM_DRIVER_ARGUMENTS': ['--headless']
+    }
+
+    def start_requests(self):
+        urls = ['https://www.daraz.pk/smartphones/?page=2']
+        for url in urls:
+            yield SeleniumRequest(
+                url=url,
+                callback=self.parse,
+                wait_time=10)
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'author': quote.xpath('span/small/text()').get(),
-                'text': quote.css('span.text::text').get(),
-            }
+        
+        
+        # print(response.selector.xpath("//div[@class='c16H9d']/a/text()").extract())
 
-        next_page = response.css('li.next a::attr("href")').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        for mobile in response.selector.xpath("//div[@class='c16H9d']"):
+            
+            print(mobile.xpath("a/text()").extract())
+            yield {
+                'mobile_name': mobile.xpath("a/text()").extract()
+            }
+            # print(quote)
+        
+        next_page = response.css("li[title='Next Page'] a::attr('href')").get()
+        
+        # if next_page is not None:
+        yield response.follow(next_page, self.parse)
